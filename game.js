3a0450cc1f721f5fa65a452f2a2c80f36af81e02
ReedsmCBA <shaun.reed@cba.com.au>
2024-11-06 08:05:09 +1100

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ let availableQuesions = [];
 let questions = [];
 
 fetch(
-    'https://opentdb.com/api.php?amount=20&category=22&difficulty=easy&type=multiple'
+    'question.json'
 )
     .then((res) => {
         return res.json();
